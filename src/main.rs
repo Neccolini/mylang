6ffd5c4aa2e819262376e1af9594cf45351ac95d
Neccolini <shun11202991@gmail.com>
2021-05-12 22:34:08 +0900

    ファイル配置を整えた

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
-pub mod llvmir;
+mod compiler;
+use compiler::*;
+
+use std::env;
+use std::fs;
+#[allow(dead_code)]
 fn main() {
-    let text = "Hello world";
 
+    let args: Vec<String> = env::args().collect();
 
+    if args.len() == 1 {
+        println!("identify the filename to parse");
+        std::process::exit(1);
+    }
+
+    // todo(入力ファイルが大きいと失敗する可能性がある)
+    let text = match fs::read_to_string(&args[1]) {
+        Ok(n) => n,
+        Err(err) => {
+            println!("error : {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let token_list = tokenizer::tokenize(&mut text.chars());
+    let expr_list = parser::token_to_expr(&token_list);
+    for expr in expr_list {
+        println!("{:?}", expr);
+    }
 }
